c468e98b8b11f7e3454c2c899744c6dd01d3eb49
Suramusic <123370264+Suramusic@users.noreply.github.com>
2023-02-15 14:13:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import getenv
 
 LOAD = getenv("LOAD", "False").split()
 
-NO_LOAD = getenv("NO_LOAD", "False").split()
+NO_LOAD = getenv("NO_LOAD", "").split()
 
 TOKEN = getenv("TOKEN", "5920443941:AAG1UJMR9X7QvM-wUH617Bz8pU-MRqlpM8E")
 
